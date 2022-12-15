eb217897283b50091afe69062dfaca26838f907c
Mariya <marisha_1793@mail.ru>
2022-12-16 00:05:42 +0700

    задача 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Введите массив из 10 случайных трехзначных чисел: ");
+int[] array = CreateRandomArray();
+PrintArray(array);
+int count = NumberOfEvenNumbersInArray(array);
+Console.WriteLine(count);
+
+int NumberOfEvenNumbersInArray(int[] array)
+{
+    int sum = 0;
+   
+        for (int i = 0; i < array.Length; i++)
+    {
+        int numberremains = array[i] % 2;
+        if ( numberremains == 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write(array[array.Length-1]);
+    Console.Write("]");
+}
+
+int[] CreateRandomArray()
+{
+    Random random = new Random();
+    int[] array = new int [10];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100,1000);
+    }
+    return array;
+}
